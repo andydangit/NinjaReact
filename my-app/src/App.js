0e6e145f9bdf0b4cda57dc88ecc3aabde1569ca5
Andy Dang <andy.dang.it@gmail.com>
2020-02-06 22:16:35 -0600

    13. Props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <h1> My first React App!</h1>
         <p>Welcome: :) </p> 
-        <Ninjas />
+        <Ninjas name="Ryu" age="25" belt="black"/>
       </div>
     );
   }
